8d6d4037e27b713f81fa0aa405c5e2c49125838d
Andrei <97633022+Zmei55@users.noreply.github.com>
2023-03-05 21:32:30 +0100

    update

--- src/Data/servicesList.ts
+++ src/Data/servicesList.ts
@@ -22,4 +22,52 @@ export const servicesList: TServicesList = [
     name: "Immunglobulin G (IgG)",
     codeService: "L18.36.00.0.004",
   },
+  {
+    name: "Enzyme",
+    codeService: "L18.36.00.0.005",
+  },
+  {
+    name: "Gerinnung",
+    codeService: "L18.36.00.0.006",
+  },
+  {
+    name: "Differentialblutbild",
+    codeService: "L18.36.00.0.007",
+  },
+  {
+    name: "Entzündungsfaktoren",
+    codeService: "L18.36.00.0.008",
+  },
+  {
+    name: "Blutgasanalyse (BGA)",
+    codeService: "L18.36.00.0.009",
+  },
+  {
+    name: "Hormone und Diabetes mellitus",
+    codeService: "L18.36.00.0.010",
+  },
+  {
+    name: "Leber",
+    codeService: "L18.36.00.0.011",
+  },
+  {
+    name: "Tumormarker",
+    codeService: "L18.36.00.0.012",
+  },
+  {
+    name: "Proteine",
+    codeService: "L18.36.00.0.013",
+  },
+  {
+    name: "Blutgerinnung",
+    codeService: "L18.36.00.0.014",
+  },
+  {
+    name: "Blutserums",
+    codeService: "L18.36.00.0.015",
+  },
+  {
+    name: "Blutkultur",
+    codeService: "L18.36.00.0.016",
+  },
 ];
