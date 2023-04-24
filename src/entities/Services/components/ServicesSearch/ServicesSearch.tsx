bf8f280c04a53607b1082707138797669baf7d8b
Andrei <97633022+Zmei55@users.noreply.github.com>
2023-04-24 22:25:49 +0200

    SSModal

--- src/entities/Services/components/ServicesSearch/ServicesSearch.tsx
+++ src/entities/Services/components/ServicesSearch/ServicesSearch.tsx
@@ -1,117 +1,18 @@
-import {
-  ServicesSearchModalBtn,
-  ServicesSearchModalHeader,
-  ServicesSearchFilter,
-  ServicesSearchSelectedList,
-  useServicesFilter,
-  useAddService,
-  useToggleModal,
-  deleteService,
-  handleClearForm,
-  IService,
-} from "entities/Services";
-import { Modal, useInput, Button as ResetBtn, Button as SaveBtn } from "shared";
+import { ServicesSearchModalBtn, ServicesSearchModal } from "entities/Services";
+import { useToggle } from "shared";
 
-import {
-  ModalBody,
-  ServiceForm,
-  SelectedServicesBox,
-  SelectedServicesTitle,
-  ButtonsBox,
-} from "./ServicesSearch.styled";
+// import {} from "./ServicesSearch.styled";
 
-interface IServicesSearch {
-  setSavedServicesList(e: IService[]): void;
-}
+interface IServicesSearch {}
 
-export const ServicesSearch = ({ setSavedServicesList }: IServicesSearch) => {
-  const [filter, setFilter, handleFilter] = useInput("");
-  const { visibleServices, setVisibleServices } = useServicesFilter(filter, []);
-  const { selectedServices, setSelectedServices, addService } = useAddService(
-    []
-  );
-  const { showModal, toggleModal } = useToggleModal(false);
-
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> = event => {
-    event.preventDefault();
-
-    setSavedServicesList(selectedServices);
-    setFilter("");
-    setVisibleServices([]);
-    setSelectedServices([]);
-    toggleModal(setFilter, setVisibleServices);
-  };
+export const ServicesSearch: React.FC<IServicesSearch> = () => {
+  const [showModal, toggleModal] = useToggle(false);
 
   return (
     <>
-      <ServicesSearchModalBtn
-        toggleModal={toggleModal}
-        setFilter={setFilter}
-        setVisibleServices={setVisibleServices}
-      />
+      <ServicesSearchModalBtn toggleModal={toggleModal} />
 
-      {showModal && (
-        <Modal
-          width="1574px"
-          height="890px"
-          onClose={() => toggleModal(setFilter, setVisibleServices)}
-        >
-          <ServicesSearchModalHeader
-            toggleModal={toggleModal}
-            setFilter={setFilter}
-            setVisibleServices={setVisibleServices}
-          />
-
-          <ModalBody>
-            <ServiceForm onSubmit={handleSubmit}>
-              <ServicesSearchFilter
-                filter={filter}
-                handleFilter={handleFilter}
-                visibleServices={visibleServices}
-                addService={addService}
-              />
-
-              <SelectedServicesBox>
-                <SelectedServicesTitle>
-                  Ausgewählte Dienste
-                </SelectedServicesTitle>
-
-                <ServicesSearchSelectedList
-                  selectedServices={selectedServices}
-                  deleteService={deleteService}
-                  setSelectedServices={setSelectedServices}
-                />
-
-                <ButtonsBox>
-                  <ResetBtn
-                    width="208px"
-                    height="72px"
-                    background="grey"
-                    onClick={() =>
-                      handleClearForm(
-                        setFilter,
-                        setVisibleServices,
-                        setSelectedServices
-                      )
-                    }
-                  >
-                    Stornieren
-                  </ResetBtn>
-
-                  <SaveBtn
-                    type="submit"
-                    width="318px"
-                    height="72px"
-                    background="blue"
-                  >
-                    Speichern und schließen
-                  </SaveBtn>
-                </ButtonsBox>
-              </SelectedServicesBox>
-            </ServiceForm>
-          </ModalBody>
-        </Modal>
-      )}
+      {showModal && <ServicesSearchModal toggleModal={toggleModal} />}
     </>
   );
 };
---
+++ src/entities/Services/components/ServicesSearchModal/ServicesSearchModal.tsx
@@ -0,0 +1,100 @@
+import {
+  ServicesSearchModalHeader,
+  ServicesSearchFilter,
+  ServicesSearchSelectedList,
+  deleteService,
+  handleClearForm,
+  useAddService,
+  useSavedServicesList,
+  useServicesFilter,
+} from "entities/Services";
+import {
+  useInput,
+  useAppSelector,
+  Modal,
+  Button as ResetBtn,
+  Button as SaveBtn,
+} from "shared";
+
+import {
+  ModalBody,
+  ServiceForm,
+  SelectedServicesBox,
+  SelectedServicesTitle,
+  ButtonsBox,
+} from "./ServicesSearchModal.styled";
+
+interface IServicesSearchModal {
+  toggleModal(): void;
+}
+
+export const ServicesSearchModal: React.FC<IServicesSearchModal> = ({
+  toggleModal,
+}) => {
+  // const servicesList = useAppSelector(state => state.services.services);
+  const [filter, setFilter, handleFilter] = useInput("");
+  const { visibleServices, setVisibleServices } = useServicesFilter(filter, []);
+  const { selectedServices, setSelectedServices, addService } = useAddService(
+    []
+  );
+  const { setSavedServicesList } = useSavedServicesList([]);
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = event => {
+    event.preventDefault();
+
+    setSavedServicesList(selectedServices);
+  };
+
+  return (
+    <Modal width="1574px" height="890px" onClose={() => toggleModal()}>
+      <ServicesSearchModalHeader toggleModal={toggleModal} />
+
+      <ModalBody>
+        <ServiceForm onSubmit={handleSubmit}>
+          <ServicesSearchFilter
+            filter={filter}
+            handleFilter={handleFilter}
+            visibleServices={visibleServices}
+            addService={addService}
+          />
+
+          <SelectedServicesBox>
+            <SelectedServicesTitle>Ausgewählte Dienste</SelectedServicesTitle>
+
+            <ServicesSearchSelectedList
+              selectedServices={selectedServices}
+              deleteService={deleteService}
+              setSelectedServices={setSelectedServices}
+            />
+
+            <ButtonsBox>
+              <ResetBtn
+                width="208px"
+                height="72px"
+                background="grey"
+                onClick={() =>
+                  handleClearForm(
+                    setFilter,
+                    setVisibleServices,
+                    setSelectedServices
+                  )
+                }
+              >
+                Abbruch
+              </ResetBtn>
+
+              <SaveBtn
+                type="submit"
+                width="318px"
+                height="72px"
+                background="blue"
+              >
+                Speichern und schließen
+              </SaveBtn>
+            </ButtonsBox>
+          </SelectedServicesBox>
+        </ServiceForm>
+      </ModalBody>
+    </Modal>
+  );
+};
