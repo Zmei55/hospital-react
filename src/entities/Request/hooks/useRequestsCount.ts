1622bdfd424a877f049431bf4842197f6c88332c
Andrei <97633022+Zmei55@users.noreply.github.com>
2023-10-27 16:07:42 +0200

    clg deleted

--- src/entities/Request/hooks/useRequestsCount.ts
+++ src/entities/Request/hooks/useRequestsCount.ts
@@ -11,7 +11,6 @@ export const useRequestsCount = () => {
   const getRequestsCount = async () => {
     try {
       const count = await requestsCount(null).unwrap();
-      console.log("getRequestsCount ~ count:", count);
       dispatch(addRequestDBCount(count + 1));
     } catch (error) {
       console.log("ERROR fetchRequestCount");
