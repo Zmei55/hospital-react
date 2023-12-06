a3c0dd990ba827bbb0b7b296be311d00eea07cdd
Andrei <97633022+Zmei55@users.noreply.github.com>
2023-12-06 11:55:05 +0100

    test files refactoring

---
+++ src/tests/mocks/userMocks.ts
@@ -0,0 +1,15 @@
+import { IUserMocks } from "tests/types";
+
+export const user: IUserMocks = {
+  _id: 1,
+  username: "test",
+  password: "test123",
+  name: "Mister Muster",
+  role: "USER",
+  workplace: "SURGERY__TREATMENT_ROOM",
+  position: "Krankenschwester",
+  isNotLocked: true,
+  createdAt: "2023-12-05T18:54:19.944+00:00",
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiIxIn0.VxV5xaU0Hh6HFqLbxKwqAf0xw5W69QzR640p-SEgwek",
+};
---
+++ src/tests/types/IUserMocks.ts
@@ -0,0 +1,12 @@
+export interface IUserMocks {
+  _id: string | number;
+  username: string;
+  password: string;
+  name: string;
+  workplace: string;
+  position: string;
+  role: string;
+  isNotLocked: boolean;
+  token: string;
+  createdAt: string;
+}
